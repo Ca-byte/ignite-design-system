fde96089e6c7a2f86ffc492f580397c48cccdb8a
Caroline Vieira <carolinesanvieira@gmail.com>
2023-04-12 19:10:33 +0200

    feat: delete stories default - create Button componnet

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,15 +1,26 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
-    "builder": "storybook-vite"
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
   }
 }
