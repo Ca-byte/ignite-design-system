c1ece161a171b109dc8ea2d27cdb64aeb94746da
Caroline Vieira <carolinesanvieira@gmail.com>
2023-04-20 15:53:55 +0200

    feat: Design System Components - Heading component

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+
+  args: {
+    children: 'Costum title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default Heading will always be an `H2` but can be changed with the `as` property',
+      },
+    },
+  },
+}
