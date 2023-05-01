0aefbbccff2ec143ad6a0cedddfd6ed299d955c5
Caroline Vieira <carolinesanvieira@gmail.com>
2023-05-01 19:45:55 +0200

    ci: change config.base

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,6 +19,7 @@ module.exports = {
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/ignite-design-system/'
+      
     }
 
     return config
